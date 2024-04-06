a66d364151e728706ed51076a87d7bc2f912fcea
Sanjeev Kumar <sanjeevscet@gmail.com>
2024-04-06 22:05:18 +0530

    main Adding constants for Addresses

--- common/constant.go
+++ common/constant.go
@@ -1,5 +1,11 @@
 package common
 
+const (
+	RestAPI            = ":1414"
+	UserServiceAddress = "localhost:50052"
+	PostServiceAddress = "localhost:50053"
+)
+
 var ValidRoles = []string{
 	"admin",
 	"editor",
