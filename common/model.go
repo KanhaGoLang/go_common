8bd1e48d038a57da029986868e1b0d7f8500439f
Sanjeev Kumar <sanjeevscet@gmail.com>
2024-04-08 17:32:26 +0530

    main Removing password while transforming to json

--- common/model.go
+++ common/model.go
@@ -4,7 +4,7 @@ type User struct {
 	Id       int32  `json:"id"`
 	Name     string `json:"name" validate:"required,min=3,max=20"`
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,strength"`
+	Password string `json:"-" validate:"required,strength"`
 	Role     string `json:"role" validate:"required,validateRole"`
 	IsActive bool   `json:"is_active"`
 }
